979969cecdf0cc61ccb9615a8f246751eb207368
jorgecruces78-svg <jorgecruces78@gmail.com>
2026-09-10 18:51:58 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+// Service Worker - Himnario Digital
+// Permite que la app funcione sin conexión una vez visitada la primera vez.
+
+const CACHE_NAME = 'himnario-mqv-v15';
+const FILES_TO_CACHE = [
+  './himnario-digital.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
